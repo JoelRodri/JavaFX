18452c233fd73929848c1db1b05c773043a277e0
jherna <jherna76@elpuig.xeill.net>
2022-01-14 17:28:54 +0100

    first commmit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module mp3.uf5.dam.tutorialfxml {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens mp3.uf5.dam.tutorialfxml to javafx.fxml;
+    exports mp3.uf5.dam.tutorialfxml;
+    exports mp3.uf5.dam.tutorialfxml.control;
+    opens mp3.uf5.dam.tutorialfxml.control to javafx.fxml;
+}
