c03ba90134aa8b865967e9b8c0333c89052ddade
jrodri <jrodri@elpuig.xeill.net>
2022-02-08 16:02:58 +0100

    prueba

--- src/main/java/mp3/uf5/dam/tutorialfxml/control/RootLayoutControler.java
+++ src/main/java/mp3/uf5/dam/tutorialfxml/control/RootLayoutControler.java
@@ -10,16 +10,13 @@ public class RootLayoutControler {
 
     Scene scene;
     Pane pane;
-    public void cambiarStile(ActionEvent actionEvent) throws IOException {
-
+    public void cambiarOscuro() {
         pane.getStylesheets().add("DarkTheme.css");
+    }
 
-//        Parent root;
-//        root = (AnchorPane) FXMLLoader.load(getClass().getResource("CssChange.fxml"));
-//        String css = CssChange.class.getResource("login2.css").toExternalForm();
-//        root.getStylesheets().clear();
-//        root.getStylesheets().add(css);
-//        root.applyCss();
+    public void cambiarClaro(){
+        pane.getStylesheets().remove(pane.getStylesheets().size()-1);
+//        pane.getStylesheets().add("WitheTheme.css");
     }
 
     public void setScene(Scene scene, Pane pane) {
